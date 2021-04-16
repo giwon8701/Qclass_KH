284379b10f8b3e0dfd68ec04fe53a0aaf9374f38
Projects - Qclass_KH <l4279625@gmail.com>
2021-04-16 16:51:08 +0900

    스프링5일차

--- Workspace_Spring/Spring04/src/com/test01/BeanTest.java
+++ Workspace_Spring/Spring04/src/com/test01/BeanTest.java
@@ -1,7 +1,11 @@
 package com.test01;
 
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class BeanTest {
 	
@@ -43,9 +47,32 @@ public class BeanTest {
 		}
 	}
 	public void setList(List<String> list) {
-		System.out.println("setArray(String[] arr) 호출");
+		System.out.println("setList(List<String> list) 호출");
 		for (String s : list) {
 			System.out.println(s);
+		}
+	}
+	
+	public void setSet(Set<String> set) {
+		System.out.println("setSet(Set<String> set) 호출!");
+		for (String s : set) {
+			System.out.println(s);
+		}
+	}
+	
+	public void setMap(Map<Integer, String> map) {
+		System.out.println("setMap(Map<Integer, String> map) 호출!");
+		// Entry 써서 k : v 형태로 출력하자
+		Set<Map.Entry<Integer, String>> entrySet = map.entrySet();
+		for (Entry<Integer, String> entry : entrySet) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
+	}
+	
+	public void setScore (List<Score> list) {
+		System.out.println("setScore(List<Score> list) 호출");
+		for (Score sc : list) {
+			System.out.println(sc);
 		}
 	}
 
