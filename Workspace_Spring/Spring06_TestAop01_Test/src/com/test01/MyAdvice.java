28f88a89fc1d9a65417c226349329cfb6c0d1767
Projects - Qclass_KH <l4279625@gmail.com>
2021-04-20 02:06:55 +0900

    Spring Proxy Homework

---
+++ Workspace_Spring/Spring06_TestAop01_Test/src/com/test01/MyAdvice.java
@@ -0,0 +1,30 @@
+package com.test01;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class MyAdvice {
+	 	
+	public MyAdvice() {}
+
+	@Before("execution(public * sayName(..))")
+	public void beforeSaying(JoinPoint join) {
+		System.out.println("당신의 이름은 무엇입니까?");
+	}
+	
+	@After("execution(public * sayName(..))")
+	public void afterSaying(JoinPoint join) {
+		System.out.println("이름이 멋지시네요.");
+		System.out.println("직업이 무엇입니까?");
+	}
+
+	@AfterReturning(pointcut = "execution(public * sayJob(..))", returning = "returnVal")
+	public void afterReturnSaying(JoinPoint join, Object returnVal) {
+		System.out.println("나의 직업은 " + returnVal + " 입니다.");
+	}
+
+}
