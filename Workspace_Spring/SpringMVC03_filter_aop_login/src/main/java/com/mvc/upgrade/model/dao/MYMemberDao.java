08ddedd77a5df44dc98c52e3dc3acbe701541aa8
Projects - Qclass_KH <l4279625@gmail.com>
2021-04-27 15:49:12 +0900

    스프링 로그인/회원가입 구현

---
+++ Workspace_Spring/SpringMVC03_filter_aop_login/src/main/java/com/mvc/upgrade/model/dao/MYMemberDao.java
@@ -0,0 +1,12 @@
+package com.mvc.upgrade.model.dao;
+
+import com.mvc.upgrade.model.dto.MYMemberDto;
+
+public interface MYMemberDao {
+	
+	String NAMESPACE = "mymember.";
+	
+	public MYMemberDto login(MYMemberDto dto);
+	public int regist(MYMemberDto dto);
+
+}
